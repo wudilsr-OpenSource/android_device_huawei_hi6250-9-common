7b913e29f5130f8860247c8b4c5dfa2ec23fd56c
wudilsr <wudilsr@gmail.com>
2023-01-07 01:38:54 +0000

    hi6250-9-commoon: Move manifests to hidl/vintf folder

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,11 +43,11 @@ BOARD_VENDOR := huawei
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
+    $(COMMON_PATH)/hidl/vintf/device_framework_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/hidl/vintf/manifest.xml
+DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/vintf/compatibility_matrix.xml
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG := true
