a61bf762bf3ea9988425f8a785b7eccfc4ca2317
A2L5E0X1 <alexed25@protonmail.com>
2024-01-30 23:48:29 +0100

    hi6250-9-common: Copy default BCM wpa overlay configs

--- common.mk
+++ common.mk
@@ -150,7 +150,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
-    wpa_supplicant
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+-include hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/huawei/hi6250-9-common/hi6250-9-common-vendor.mk)
