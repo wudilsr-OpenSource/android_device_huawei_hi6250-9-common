433f408270cffbc6ca38bf1d1ef22be11d67da9d
Bruno Martins <bgcngm@gmail.com>
2024-08-23 04:12:33 +0200

    hi6250-9-common: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- common.mk
+++ common.mk
@@ -84,10 +84,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-
-PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
 
 PRODUCT_PACKAGES += \
