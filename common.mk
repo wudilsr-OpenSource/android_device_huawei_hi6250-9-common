8876b72bf46e18497ea03ff4c594a620f968d918
wudilsr <wudilsr@gmail.com>
2024-12-24 16:25:24 +0800

    hi6250-9-common: Build USB service and Tethering

--- common.mk
+++ common.mk
@@ -347,6 +347,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service \
+    com.android.future.usb.accessory
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
