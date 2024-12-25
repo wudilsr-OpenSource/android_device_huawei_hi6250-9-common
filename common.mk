379bce2bf9491704be3f5325e92e62c4d2f0da2a
wudilsr <wudilsr@gmail.com>
2024-12-25 16:05:45 +0800

    hi6250-9-common: Add public.libraries.txt

--- common.mk
+++ common.mk
@@ -279,6 +279,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.2.vendor \
