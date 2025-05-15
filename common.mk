95825e9fafdb755cd662251aefd501ff78f48fd9
aoleary <seanm187@gmail.com>
2025-05-15 16:20:49 +0800

    hi6250-9-common: build android.hardware.sensors@2.0-1
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.sensors@2.1::ISensors/default in either framework or device VINTF manifest.
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.sensors@2.0::ISensors/default in either framework or device VINTF manifest.

--- common.mk
+++ common.mk
@@ -237,7 +237,9 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1
     
 # For GNSS
 PRODUCT_PACKAGES += \
