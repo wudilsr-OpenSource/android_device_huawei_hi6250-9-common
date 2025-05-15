60227854535f43cabc6a902a7127536a6e955198
wudilsr <wudilsr@gmail.com>
2025-05-15 16:23:31 +0800

    hi6250-9-common: Build Motorola Health
    hwservicemanager: getTransport: Cannot find entry motorola.hardware.health@1.0::IMotHealth/default in either framework or device VINTF manifest.

--- common.mk
+++ common.mk
@@ -261,6 +261,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+
+# Motorola Health
+PRODUCT_PACKAGES += \
+    motorola.hardware.health@1.0.vendor
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
