038f0192f103060cadb986d6df667f1255477516
sashascurtu <sasha.scurtu@gmail.com>
2025-05-15 16:12:18 +0800

    hi6250-9-common: Build mock power stats HAL Fixes the following: * Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device VINTF manifest. * BatteryStatsService: Unable to load Power.Stats.HAL. Setting rail availability to false [akmacc] Add sepolicy: Label Signed-off-by: akmacc <akjagxz@outlook.com>

--- common.mk
+++ common.mk
@@ -23,7 +23,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-     android.hardware.power-service.pixel-libperfmgr
+    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Control groups and task profiles (vndk 28)
 PRODUCT_COPY_FILES += \
